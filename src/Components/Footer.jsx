8bba1d6c140b5c41e223a8dc352fb93becc8c857
Katalin Kovács <katalin.kovacs@innosw.hu>
2021-10-13 09:05:36 +0200

    recolor footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,9 +6,8 @@ const Footer = () => {
             <footer className="footer body">
                 <div className="container-fluid">
                     <div className="row">
-                        <div className="col-12 text-white bg-secondary">
-                            <br />
-                            <p className="lead text-center pt-1 fs-6">
+                        <div className="col-12 text-white footer-color bg-gradient">
+                            <p className="lead text-center pt-1 fs-6 mt-4">
                                 Copyright © Katt 2021
                             </p>
                         </div>
